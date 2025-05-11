7d9d81f54b784cd843a8b3559683743a00e17a05
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2025-05-11 21:51:00 +0530

    update

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -8,19 +8,19 @@ const Gallery = () => {
   // Example photos (replace with actual photos of you and Alina)
   const photos = [
     {
-      url: 'birthday/src/components/sections/photos/IMG-20240302-WA0017.jpg',
+      url: '/photos/IMG-20240302-WA0017.jpg',
       caption: 'Your cutu baby pic(awww my love)❤️',
     },
     {
-      url: 'birthday/src/components/sections/photos/IMG-20231221-WA0000.jpg',
+      url: '/photos/IMG-20231221-WA0000.jpg',
       caption: 'Making silly faces together',
     },
     {
-      url: 'birthday/src/components/sections/photos/Snapchat-1427700348.mp4',
+      url: '/photos/Snapchat-1427700348.mp4',
       caption: 'That beautiful Face I fell in love with',
     },
     {
-      url: 'birthday/src/components/sections/photos/VID_20250510_111040_045.mp4',
+      url: '/photos/VID_20250510_111040_045.mp4',
       caption: 'Your smile that lights up my world',
     }
   ]
